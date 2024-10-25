ece775e4b8200c9c580cd3610c6a37c0b5b9ff9e
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2024-10-25 11:55:16 -0300

    Criando a estrutura da biblioteca.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="DOT_logger",
+    version="1.0.0",
+    description="Biblioteca de logs para python.",
+    long_description="Uma pequena biblioteca de logs para python.",
+    author="Gilkleber Medeiros de Araújo",
+    packages=["DOT_logger"],
+)
