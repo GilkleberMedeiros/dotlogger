ccc5038cbc7722b725b8b108a78a610349c3a21c
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2024-12-12 15:27:36 -0300

    Testes da função log realizados, partindo para transforma-lá na classe final.

--- tests/dotlogger_temporary_test.py
+++ tests/dotlogger_temporary_test.py
@@ -12,3 +12,16 @@ def test():
 
     block_log_by_classifier("set2", "set")
     log("set2", "class3", "10.5", "Menssagem de teste 3", "PROCESS")
+
+    write_all_logs_to("./other_logs/abc.logs")
+
+    log("set3", "class4", "5.25", "Menssagem de teste 4", "PROCESS")
+    log("set3", "class4", "2.125", "Menssagem de teste 5", "PROCESS")
+
+    log("set4", "class5", "1", "Menssagem de teste 6", "PROCESS", write_to="./abc.logs")
+
+    log("set5", "class6", "2", "Menssagem de teste 4", "PROCESS", include_date=False)
+    log("set5", "class6", "2", "Menssagem de teste 4", "PROCESS", include_time=False)
+    log("set5", "class6", "2", "Menssagem de teste 4", "PROCESS", in_location="def")
+    log("set5", "class6", "2", "Menssagem de teste 4", "PROCESS", on_resource="brade")
+
