51ec5326e0dce26bf2605022215c72343b1bfe31
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2024-12-13 16:36:30 -0300

    modificando os testes temporários.

--- tests/dotlogger_temporary_test.py
+++ tests/dotlogger_temporary_test.py
@@ -2,26 +2,39 @@ from ..dotlogger import *
 from typing import Any
 
 
-# TODO: Realizar os testes da função log
-
 def test():
+    print("test func executed")
+    logger = DotLogger("set1", "class1")
+
     write_all_logs_to("./logs/")
 
-    log("set1", "class1", "42", "Menssagem de teste 1", "ERROR")
-    log("set1", "class2", "21", "Menssagem de teste 2", "WARNING")
+    logger.log("42", "Menssagem de teste 1", "ERROR")
+    logger.log("21", "Menssagem de teste 2", "WARNING")
 
     block_log_by_classifier("set2", "set")
-    log("set2", "class3", "10.5", "Menssagem de teste 3", "PROCESS")
+    logger.set = "set2"
+    logger.log("10.5", "Menssagem de teste 3", "PROCESS")
+    
+    logger.set = "set3"
+    write_all_logs_to("./other_logs/abc.txt")
 
-    write_all_logs_to("./other_logs/abc.logs")
+    logger.log("5.25", "Menssagem de teste 4", "PROCESS")
+    logger.log("2.125", "Menssagem de teste 5", "PROCESS")
 
-    log("set3", "class4", "5.25", "Menssagem de teste 4", "PROCESS")
-    log("set3", "class4", "2.125", "Menssagem de teste 5", "PROCESS")
+    logger.log("1", "Menssagem de teste 6", "PROCESS", write_to="./abc.txt")
 
-    log("set4", "class5", "1", "Menssagem de teste 6", "PROCESS", write_to="./abc.logs")
+    logger.log("2", "Menssagem de teste 4", "PROCESS", include_date=False)
+    logger.log("2", "Menssagem de teste 4", "PROCESS", include_time=False)
+    logger.log("2", "Menssagem de teste 4", "PROCESS", in_location="def")
+    logger.log("2", "Menssagem de teste 4", "PROCESS", on_resource="brade")
 
-    log("set5", "class6", "2", "Menssagem de teste 4", "PROCESS", include_date=False)
-    log("set5", "class6", "2", "Menssagem de teste 4", "PROCESS", include_time=False)
-    log("set5", "class6", "2", "Menssagem de teste 4", "PROCESS", in_location="def")
-    log("set5", "class6", "2", "Menssagem de teste 4", "PROCESS", on_resource="brade")
 
+class Test:
+    def test():
+        logger = DotLogger("set1", "class1")
+
+        write_all_logs_to("./new_logs/")
+        print(Path("./new_logs/").absolute())
+
+        logger.log("id1", "Log chamado diretamente de uma classe 1", "Process")
+        logger.log("id2", "Log chamado diretamente de uma classe 2", "Process")
